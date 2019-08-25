b215d3d4f6e016ae59cfd7abd5119522dc584569
Ashok Mamnani <ashok.mamnani01@gmail.com>
2019-08-25 20:32:51 +0530

    Swapping with diffrent methods

--- PART-I/PRO-2.py
+++ PART-I/PRO-2.py
@@ -5,5 +5,32 @@ b=float(input("Enter value of B:"))
 a=a+b
 b=a-b
 a=a-b
+print("METHOD 1")
+print(f"A:{a}")
+print(f"B:{b}")
+
+
+#METHOD 2
+a=a*b
+b=a/b
+a=a/b
+print("METHOD 2")
+print(f"A:{a}")
+print(f"B:{b}")
+
+#METHOD 3 work only with int value
+a=int(a)
+b=int(b)
+a=a^b
+b=a^b
+a=a^b
+print("METHOD 3")
+print(f"A:{a}")
+print(f"B:{b}")
+
+
+#METHOD 4 
+a,b=b,a
+print("METHOD 4")
 print(f"A:{a}")
 print(f"B:{b}")
